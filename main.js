6da5bd19b316224852a3e4c51ac752b90d6ed950
Alisa Leung <alisal.singyee@gmail.com>
2025-12-24 14:37:42 -0800

    donut and stars but animate function is glitchy

--- main.js
+++ main.js
@@ -1,25 +1,51 @@
+import './style.css';
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-const renderer = new THREE.WebGLRenderer();
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+});
 renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setAnimationLoop( animate );
+renderer.render(scene, camera);
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial ( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const texture = new THREE.TextureLoader().load('assets/iceCubeTex.jpg');
+const material = new THREE.MeshBasicMaterial ({map: texture});
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 
-camera.position.z = 5;
+const donut_geo = new THREE.TorusGeometry(10, 3, 16, 100);
+const donut_tex = new THREE.MeshBasicMaterial({color: 0xffffff});
+const donut = new THREE.Mesh(donut_geo, donut_tex);
+scene.add(donut);
+
+camera.position.z = 18;
 
 function animate() {
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    donut.rotation.x += 0.01;
+    donut.rotation.y += 0.01;
 
-    renderer.render( scene, camera );
+    renderer.render(scene, camera);
     requestAnimationFrame(animate);
 }
 
 animate();
+
+function add_star(){
+    const star_geometry = new THREE.SphereGeometry(0.25, 24, 25);
+    const star_material = new THREE.MeshBasicMaterial({color: 0xffffff});
+    const star = new THREE.Mesh(star_geometry, star_material);
+
+    const [x, y, z] = Array(3)
+        .fill()
+        .map(() => THREE.MathUtils.randFloatSpread(200));
+
+    star.position.set(x, y, z);
+    scene.add(star);
+}
+
+Array(200).fill().forEach(add_star);
