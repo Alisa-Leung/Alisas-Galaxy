1f8559636440cc85ac90d48212df130851ab7222
Alisa Leung <alisal.singyee@gmail.com>
2025-12-24 14:14:18 -0800

    fixed rendering typos

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 import * as THREE from 'three';
 
-const scene = new THREE.scene();
+const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window,innerHeight );
+renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
@@ -19,7 +19,7 @@ function animate() {
     cube.rotation.y += 0.01;
 
     renderer.render( scene, camera );
-    requestAnimationFrame(animae);
+    requestAnimationFrame(animate);
 }
 
 animate();
